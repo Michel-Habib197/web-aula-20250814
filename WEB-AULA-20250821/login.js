4af2a1b8443560352a20f6221a6dbfa617f2f473
Michel-Habib197 <126814378+Michel-Habib197@users.noreply.github.com>
2025-08-28 14:38:59 +0000

    loginJS

---
+++ WEB-AULA-20250821/login.js
@@ -0,0 +1,22 @@
+const email = "admin@email.com";
+const senha = "admin";
+
+function verificarLogin(){
+    const emailInformado = document.getElementById("email").value;
+    const senhaInformada = document.getElementById("senha").value;
+
+    if(emailInformado === email){
+        alert("Email informado corretamente!");
+        if(senhaInformada === senha){
+            alert("Senha informada corretamente!");
+            window.location = "home.html"
+        }
+        else{
+            alert("Senha incorreta!");
+        }  
+    }
+    else{
+        alert("Email incorreto!");
+    }
+}
+
