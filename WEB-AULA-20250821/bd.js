efc36ccfc18d67fba59fbc432f03505ed879b6ce
Michel-Habib197 <126814378+Michel-Habib197@users.noreply.github.com>
2025-09-18 14:49:27 +0000

    json

--- WEB-AULA-20250821/bd.js
+++ WEB-AULA-20250821/bd.js
@@ -1,4 +1,37 @@
 const usuario = {
     "email": "admin@gmail.com",
-    "senha": "admin",
+    "senha": "admin"
+}
+
+const dados ={
+    [
+        "id": 1,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "img/onibus.png"
+    ], 
+    [
+        "id": 2,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "img/tecnologia6g.png"
+    ],
+    [
+        "id": 3,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "img/festivalMusica.png"
+    ]
 }
--- WEB-AULA-20250821/login.js
+++ WEB-AULA-20250821/login.js
@@ -6,9 +6,9 @@ function verificarLogin(){
     const senhaInformada = document.getElementById("senha").value;
 
     if(emailInformado === email){
-        alert("Email informado corretamente!");
+        // alert("Email informado corretamente!");
         if(senhaInformada === senha){
-            alert("Senha informada corretamente!");
+            // alert("Senha informada corretamente!");
             window.location = "home.html"
         }
         else{
